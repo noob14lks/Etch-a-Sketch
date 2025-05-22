d3274e8688a5024be936db75e3bf7f39f57a9a28
Lahari Keethisri <laharikeerthisg@gmail.com>
2025-05-22 16:39:20 +0530

    Added sketchpad whose pixels can be adjusted and added a

---
+++ README.md
@@ -0,0 +1,21 @@
+## Etch-a-Sketch
+
+This is a simple Etch-a-Sketch created using HTML, CSS and JavaScript.
+
+## How to draw
+1. When the mouse hovers over the sketch pad it leaves a mark. 
+2. The pixels can be adjusted using the slider given below.
+3. Color can be picked by using color picker.
+4. Eraser can be selected by clicking on eraser button.
+5. Clear button clears the entire board.
+
+## Features
+
+1. Color can be picked based to user's choice.
+2. Eraser and clear options are available .
+3. RGB option allows users to draw using random multiple colors.
+4. Pixel adjuster is provided.
+
+---
+
+Created for learning purposes!
--- script.js
+++ script.js
@@ -1,12 +1,71 @@
 const size = document.querySelector(".num");
 const slider = document.getElementById("range");
+let space = document.querySelector(".space");
+const color = document.getElementById("picker");
+const label = document.getElementById("label");
+const rgb = document.querySelector(".RGB");
+
+createGrid(slider.value);
+
+let eraserActive = false;
+let RGBActive = false;
 
 size.innerHTML = slider.value;
 slider.addEventListener("input", () => {
   size.innerHTML = slider.value;
   const value = parseInt(slider.value);
-  for(let i = 0; i < value**2; ++i){
-    let div = document.createElement("div");
-  }
+  createGrid(value);
 });
 
+const clear = document.querySelector(".clear");
+clear.addEventListener("click", () => {
+  let divs = document.querySelectorAll(".space div");
+  divs.forEach((div) => {
+    div.style.backgroundColor = "white";
+  })
+})
+
+const eraser = document.querySelector(".eraser");
+eraser.addEventListener("click", () => {
+  eraserActive = true;
+})
+
+color.addEventListener("click", () => {
+  RGBActive = false;
+  eraserActive = false;
+})
+
+rgb.addEventListener("click", () => {
+  RGBActive = true;
+})
+
+label.addEventListener("click", () => {
+  eraserActive = false;
+  RGBActive = false;
+})
+
+function createGrid(value){
+    space.innerHTML = "";
+  for (let i = 0; i < value ** 2; ++i) {
+    let div = document.createElement("div");
+    div.style.width = Math.floor(960 / value) + "px";
+    div.style.height = Math.floor(960 / value) + "px";
+    div.style.backgroundColor = "white";
+    div.addEventListener("mouseover", () => {
+      if (eraserActive) {
+        div.style.backgroundColor = "white";
+      }
+      else if (RGBActive) {
+        const r1 = Math.floor(Math.random() * 255);
+        const r2 = Math.floor(Math.random() * 255);
+        const r3 = Math.floor(Math.random() * 255);
+        randomcolor = `rgb(${r1}, ${r2}, ${r3})`;
+        div.style.backgroundColor = randomcolor;
+      }
+      else {
+        div.style.backgroundColor = color.value;
+      }
+    })
+    space.appendChild(div);
+  }
+}
